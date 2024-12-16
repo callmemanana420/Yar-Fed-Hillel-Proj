3957ceadfe1da453bbac4f0151292b8f9a19f62f
callmemanana420 <fedoseevarik17@gmail.com>
2024-12-16 16:41:03 +0100

    fix empty list issue

--- lesson 3.2.py
+++ lesson 3.2.py
@@ -2,7 +2,8 @@ initialList = [13, 777, 666, 228, 420, 102]
 
 print(f"Before: {initialList}")
 
-initialList.insert(0, initialList[-1])
-initialList.pop()
+if len(initialList) > 0:
+    initialList.insert(0, initialList[-1])
+    initialList.pop()
 
 print(f"After: {initialList}")
